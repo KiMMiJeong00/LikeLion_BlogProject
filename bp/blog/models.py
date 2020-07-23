7c0e2bed6a5b68584d382dc321937599bd23c7b9
unknown <min7590524y@naver.com>
2020-07-23 19:40:58 +0900

    first commit

--- bp/blog/models.py
+++ bp/blog/models.py
@@ -1,10 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('data published')
     body = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
+
+
+    likes = models.ManyToManyField(User,through='Like',through_fields=('blog','user'),related_name='likes') #다대다 관계 설정를 위한 중계모델
+
+    def like_count(self): #self는 블로그 객체
+        return self.likes.count()
     
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    body = models.TextField(max_length=500)
+    pub_date = models.DateTimeField('data published')
+    c_writer = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    post = models.ForeignKey(Blog,on_delete=models.CASCADE,null=True)
+
+
+
+class Like(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
--- bp/blog/urls.py
+++ bp/blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('create', views.create, name='create'),
     path('delete/<int:blog_id>', views.delete, name='delete'),
     path('update/<int:blog_id>', views.update, name='update'),
+    path('comment/<int:blog_id>',views.comment,name='comment'),
+    path('commnet/delete/<int:comment_id>',views.comment_delete,name='comment_delete'),
+    path('like/<int:blog_id>',views.post_like,name='post_like'),#like라는 url이 들어왔을 때 post_like라는걸 만들겠다.
 ]
